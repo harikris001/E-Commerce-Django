ec1d4ddc6de0c5b267d1a03076514b30939a08c6
harikris001 <hk5983835@gmail.com>
2023-10-05 13:32:07 +0530

    Intial Commit

---
+++ core/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,redirect
+from item.models import Category, Item
+
+from . forms import SignUpForm
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(request, 'core/index.html',{
+        'categories':categories,
+        'items':items,
+    })
+
+def contact(request):
+    return render(request, 'core/contact.html',)
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form  = SignUpForm()
+
+    return render(request,'core/signup.html',{'form':form})
